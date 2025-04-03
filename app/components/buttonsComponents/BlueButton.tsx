cf4c4388e6bb51154d8baefedfb6213a7612eb4e
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2025-04-03 10:53:49 -0300

    feat: adiciona componente de Loading e atualiza BlueButton para suportar estado de carregamento

--- app/components/buttonsComponents/BlueButton.tsx
+++ app/components/buttonsComponents/BlueButton.tsx
@@ -1,33 +1,52 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import { Loading } from "../Loading/Loading";
 
 interface SendButtonProps extends TouchableOpacityProps {
   title: string;
+  isLoading?: boolean;
 }
 
-const SendButton: React.FC<SendButtonProps> = ({ title, style, ...props }) => {
+const SendButton: React.FC<SendButtonProps> = ({
+  title,
+  style,
+  isLoading = false,
+  ...props
+}) => {
   return (
-    <TouchableOpacity style={[styles.button, style]} {...props}>
-      <Text style={styles.text}>{title}</Text>
+    <TouchableOpacity
+      style={[styles.button, style, isLoading && styles.disabledButton]}
+      disabled={isLoading} //Desabilita o botão se isLoading for true
+      {...props}
+    >
+      {isLoading ? <Loading /> : <Text style={styles.text}>{title}</Text>}
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    width: '100%',
-    backgroundColor: '#1C5790',
+    width: "100%",
+    backgroundColor: "#1C5790",
     paddingVertical: 12,
     paddingHorizontal: 25,
     borderRadius: 50,
-    alignItems: 'center',
+    alignItems: "center",
   },
   text: {
-    fontFamily: 'Roboto',
-    fontWeight: 'semibold',
-    color: '#fff',
+    fontFamily: "Roboto",
+    fontWeight: "semibold",
+    color: "#fff",
     fontSize: 20,
-  }
+  },
+  disabledButton: {
+    opacity: 0.8,
+  },
 });
 
 export default SendButton;
